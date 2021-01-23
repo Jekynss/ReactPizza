5ee751be59362923e8b2906ab9dbb3fc5e609aa4
Roman Kuznetsov <Jekyns@mail.com>
2021-01-23 23:47:37 +0300

    Updates

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Button} from './Button';
-export {default as Header} from './Header';
-export {default as Categories} from './Categories';
-export {default as Popup} from './Popups';
-export {default as PizzaBlock} from './PizzaBlock';
-export {default as CartItem} from './CartItem';
-export {default as PizzaLoadingBlock} from './PizzaBlock/LoadingBlock';
